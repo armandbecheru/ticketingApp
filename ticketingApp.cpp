5deed2648393a8bf1def0bd33dd910f80c8f950d
armandbecheru <100259426+armandbecheru@users.noreply.github.com>
2023-11-24 02:21:04 +0200

    ticketingApp.cpp update
    ticketingApp.cpp has been updated in order to facilitate the testing of the Event Class in the main function

--- ticketingApp.cpp
+++ ticketingApp.cpp
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "EventLocation.h"
+#include "Event.h"
 
 
 using namespace std;
@@ -11,6 +12,7 @@ using namespace std;
 
 int main()
 {
+   //testing for the EventLocation class
     EventLocation firstLocation(1, "Arena Nationala", 57000, 40, true, 4, "Bulevardul Basarbia", "Bucharest");
     EventLocation secondLocation(2, "Stadion Arc de Triumf" , 900, 20, false, 2, "Bulevardul Kiseleff", "Bucharest");
     firstLocation.isValidLocation();
@@ -18,6 +20,20 @@ int main()
     secondLocation.isValidLocation();
     secondLocation.displayLocation();
     secondLocation.displayNumberOfLocations();
+
+    //testing for the Event class
+    Event firstEvent(1, "Concert", 2023, "December", 14, "Enjoy the music", 100, false);
+    Event secondEvent(2, "Hackathon", 2024, "January", 20, "Test your coding skills", 50, true);
+    Event thirdEvent(3, "Concert", 2023, "December", 14, "Enjoy the music", 100, false);
+
+    thirdEvent.checkForDuplicates();
+
+    firstEvent.isValidEvent();
+    firstEvent.displayEvent();
+    secondEvent.isValidEvent();
+    secondEvent.displayEvent();
+
+    secondEvent.numberOfEvents();
 
 
     return 0;
