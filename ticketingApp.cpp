fa8e4953c27a78070caae1d7fe362a1182db87c2
armandbecheru <100259426+armandbecheru@users.noreply.github.com>
2023-11-25 09:23:06 +0200

    Testing of Ticket Class via main function
    ticketingApp.cpp has been updated in order to facilitate the testing of the Ticket Class via the main function

--- ticketingApp.cpp
+++ ticketingApp.cpp
@@ -5,6 +5,7 @@
 
 #include "EventLocation.h"
 #include "Event.h"
+#include "Ticket.h"
 
 
 using namespace std;
@@ -13,6 +14,8 @@ using namespace std;
 int main()
 {
    //testing for the EventLocation class
+    cout << "-----------Testing EventLocation Class----------------------" << endl;
+
     EventLocation firstLocation(1, "Arena Nationala", 57000, 40, true, 4, "Bulevardul Basarbia", "Bucharest");
     EventLocation secondLocation(2, "Stadion Arc de Triumf" , 900, 20, false, 2, "Bulevardul Kiseleff", "Bucharest");
     firstLocation.isValidLocation();
@@ -21,7 +24,9 @@ int main()
     secondLocation.displayLocation();
     secondLocation.displayNumberOfLocations();
 
-    //testing for the Event class
+    cout << "------------Testing Event Class---------------------" << endl;
+
+    //testing for the Event class 
     Event firstEvent(1, "Concert", 2023, "December", 14, "Enjoy the music", 100, false);
     Event secondEvent(2, "Hackathon", 2024, "January", 20, "Test your coding skills", 50, true);
     Event thirdEvent(3, "Concert", 2023, "December", 14, "Enjoy the music", 100, false);
@@ -35,6 +40,14 @@ int main()
 
     secondEvent.numberOfEvents();
 
+    cout << "--------------Testing Ticket Class-------------------" << endl;
 
+    //testing for the Ticket class
+    Ticket firstTicket(1, 20, 15, 250, "Blue", "John Doe", "johndoe@example.com");
+    Ticket secondTicket(2, 10, 5, 100, "Red", "Jane Doe", "janedoe@example.com");
+    
+    firstTicket.displayTicket();
+    secondTicket.displayTicket();
+    secondTicket.getNoTickets();
     return 0;
 }
